c58769111de67a1b16498bbd281539ac83ed8fb0
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-16 16:56:42 +0200

    api data config

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const GLOBAL_VARIABLES = {
+  API_CATEGORIES_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/categories',
+  API_POSTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/posts',
+  API_COMMENTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/comments',
+  API_MEDIA_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/media',
+};
+
+export default GLOBAL_VARIABLES;
