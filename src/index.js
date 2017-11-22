a29ac4ad0b5aad8d2fd7326c14b535979b832542
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-22 12:36:52 +0200

    config router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import store from './configureStore';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
+import CategoriesCarousel from './views/CategoriesCarousel/index';
+import PostsCarousel from './views/PostsCarousel/index';
+import PostView from './views/Post/Container';
+
+import './index.css';
+
 render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App>
+        <Switch>
+          <Route exact path="/" component={CategoriesCarousel} />
+          <Route path="/categories" component={CategoriesCarousel} />
+          <Route path="/category/:categoryId" component={PostsCarousel} />
+          <Route path="/post/:postId" component={PostView} />
+        </Switch>
+      </App>
+    </HashRouter>
   </Provider>,
   document.getElementById('root'),
 );
