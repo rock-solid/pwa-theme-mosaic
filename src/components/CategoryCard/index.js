d150a32dd97b4d9dee6385458f4476bbb8203efb
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-22 12:37:50 +0200

    link categories to posts

--- src/components/CategoryCard/index.js
+++ src/components/CategoryCard/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Item, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export default class CategoryCard extends Component {
   render() {
     const { category } = this.props;
     return (
-      <div>
+      <Link to={'/category/' + category.slug + '/' + category.id}>
         <Item>
           <Item.Header as={Header}>{category.name}</Item.Header>
         </Item>
-      </div>
+      </Link>
     );
   }
 }
