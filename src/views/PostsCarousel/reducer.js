922f3f24f89709801ff9abf9e9b1009bdfbb742f
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-23 16:00:46 +0200

    define post propTypes

--- src/views/PostsCarousel/reducer.js
+++ src/views/PostsCarousel/reducer.js
@@ -1,6 +1,27 @@
 import { combineReducers } from 'redux';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 import { REQUEST_POSTS, RECEIVE_POSTS } from './action';
+
+export const postPropType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  title: PropTypes.shape({
+    rendered: PropTypes.string.isRequired,
+  }).isRequired,
+  author: PropTypes.number.isRequired,
+  categories: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  slug: PropTypes.string.isRequired,
+  content: PropTypes.shape({
+    rendered: PropTypes.string.isRequired,
+    protected: PropTypes.bool.isRequired,
+  }).isRequired,
+  excerpt: PropTypes.shape({
+    rendered: PropTypes.string.isRequired,
+    protected: PropTypes.bool.isRequired,
+  }).isRequired,
+  date: PropTypes.string.isRequired, //TO DO : date validation => class Date
+  // TO DO : proptype for image src
+});
 
 const items = (state = [], action) => {
   switch (action.type) {
