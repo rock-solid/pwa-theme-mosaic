5ba09c6f3e39c23ba8e5fead2f5672d80d8c0ae2
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-23 16:07:46 +0200

    define category propTypes

--- src/views/CategoriesCarousel/reducer.js
+++ src/views/CategoriesCarousel/reducer.js
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 import { REQUEST_CATEGORIES, RECEIVE_CATEGORIES } from './action';
 
+export const categoryPropType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+});
 const items = (state = [], action) => {
   switch (action.type) {
   case REQUEST_CATEGORIES:
