78fa45ed795de5a223f88c3f2fdaa84ac7be7a51
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-28 15:50:19 +0200

    post view styling

--- src/views/Post/PostView.js
+++ src/views/Post/PostView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Image, Header, Label } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 
 import { postPropType } from '../PostsCarousel/reducer';
 import { categoryPropType } from '../CategoriesCarousel/reducer';
@@ -8,8 +9,8 @@ import './style.css';
 
 export default class PostView extends Component {
   render() {
-    console.log('PROP', this.props);
-    const { post, categories } = this.props;
+    const { post: { _embedded: { author } }, categories, post } = this.props;
+
     return (
       <Container className="post">
         <Image src="https://placeholdit.co//i/555x650" />
@@ -20,6 +21,9 @@ export default class PostView extends Component {
             </Label>
           ))}
           <Header>{post.title.rendered}</Header>
+          <Header.Subheader>
+            &nbsp;by&nbsp;<b>{author[0].name}</b>,&nbsp;<Moment format="MMMM DD, YYYY">{post.date}</Moment>
+          </Header.Subheader>
           <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
         </Container>
       </Container>
