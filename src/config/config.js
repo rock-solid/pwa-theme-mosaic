90f65653fd7aaae619db7621867c2e3d1e413da5
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-28 15:51:14 +0200

    remove users api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,6 @@
 const GLOBAL_VARIABLES = {
   API_CATEGORIES_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/categories',
   API_POSTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/posts?_embed=users',
-  API_USERS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/users',
   API_COMMENTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/comments',
   API_MEDIA_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/media',
 };
