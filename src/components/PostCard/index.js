abaf3549cb5a818729c35a7aea704f5bce014401
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-29 16:29:45 +0200

    set post image as background

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -8,6 +8,17 @@ import './style.css';
 export default class PostCard extends Component {
   render() {
     const { post } = this.props;
+    const featuredmedia = post._embedded['wp:featuredmedia'];
+    function getImage(sourceImg) {
+      let image;
+      if (sourceImg) {
+        image = sourceImg[0].source_url;
+        return image;
+      }
+      image = 'http://via.placeholder.com/177x119';
+      return image;
+    }
+
     return (
       <Link to={'/post/' + post.slug + '/' + post.id}>
         <Item className="posts">
@@ -15,7 +26,13 @@ export default class PostCard extends Component {
             <div dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
           </Item.Header>
           <Item.Content>
-            <Item.Image size="large" src="http://via.placeholder.com/177x119" />
+            <div
+              className="image"
+              style={{
+                backgroundImage: 'url(' + getImage(featuredmedia) + ')',
+              }}
+            />
+            {/* <Item.Image size="small" src={getImage(featuredmedia)} /> */}
             <Item.Description dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
           </Item.Content>
         </Item>
