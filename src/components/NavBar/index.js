3d1ded790d34d52d9f0e681dfd111af9f80480ae
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-30 16:12:08 +0200

    create navbar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Button, Icon } from 'semantic-ui-react';
+
+import { openMenu } from './action';
+import './style.css';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.showSidebar = this.showSidebar.bind(this);
+  }
+
+  showSidebar(e) {
+    e.stopPropagation();
+    this.props.openMenu();
+  }
+
+  render() {
+    return (
+      <Button floated="right" size="tiny" className="navbar" color="teal">
+        <Icon name="content" onClick={this.showSidebar} size="large" />
+      </Button>
+    );
+  }
+}
+
+NavBar.propTypes = {
+  openMenu: PropTypes.func.isRequired,
+};
+
+export default connect(null, { openMenu })(NavBar);
