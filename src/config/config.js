0f7168cc05bc71439143a5455d55f5c5a23aa150
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-30 16:34:12 +0200

    fix pages api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const GLOBAL_VARIABLES = {
   API_CATEGORIES_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/categories',
   API_POSTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/posts?_embed=users?_embed=media',
-  API_PAGES_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/comments?_embed=media',
+  API_PAGES_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/pages?_embed=media',
   API_COMMENTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/comments',
   API_MEDIA_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/media',
 };
