5b10df0be4ac51e30a047b0cbd159bbaa1ad0801
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-30 14:55:48 +0200

    fetch pages

---
+++ src/views/SideMenu/action.js
@@ -0,0 +1,35 @@
+import config from '../../config/config';
+
+export const REQUEST_PAGES = 'REQUEST_PAGES';
+export const RECEIVE_PAGES = 'RECEIVE_PAGES';
+
+export const requestPages = () => ({
+  type: REQUEST_PAGES,
+});
+
+export const receivePages = pages => ({
+  type: RECEIVE_PAGES,
+  pages,
+});
+
+export const fetchPages = (params = {}) => (dispatch) => {
+  dispatch(requestPages());
+
+  let url;
+  if (params && params.id) {
+    url = config.API_PAGES_URL + '/' + String(params.id);
+  } else {
+    url =
+      config.API_PAGES_URL +
+      '?' +
+      Object.keys(params)
+        .map(k => k + '=' + encodeURIComponent(params[k]))
+        .join('&');
+  }
+  return fetch(url)
+    .then(response => response.json())
+    .then(json => dispatch(receivePages(json)))
+    .catch(() => {
+      dispatch(receivePages([]));
+    });
+};
---
+++ src/views/SideMenu/reducer.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import { REQUEST_PAGES, RECEIVE_PAGES } from './action';
+
+export const pagePropType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+});
+
+const items = (state = [], action) => {
+  switch (action.type) {
+  case REQUEST_PAGES:
+    return state;
+  case RECEIVE_PAGES:
+    if (Array.isArray(action.pages)) {
+      return _.unionBy(state, action.pages, 'id');
+    }
+    return _.unionBy(state, [action.pages], 'id');
+  default:
+    return state;
+  }
+};
+
+export const getPages = state => state.items;
+export default combineReducers({ items });
