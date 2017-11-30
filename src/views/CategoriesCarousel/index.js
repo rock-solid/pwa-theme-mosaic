de84893aec8c370b6a1a8e70adc750ef1b48ec28
FlorinaIe <florina.ieremciuc@gmail.com>
2017-11-30 16:12:26 +0200

    add navbar to categories carousel

--- src/views/CategoriesCarousel/index.js
+++ src/views/CategoriesCarousel/index.js
@@ -3,14 +3,25 @@ import Slider from 'react-slick';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { Sidebar } from 'semantic-ui-react';
 
 import { fetchCategories } from './action';
 import { getCategories, categoryPropType } from './reducer';
+
+import SideMenu from '../SideMenu/index';
+import NavBar from '../../components/NavBar/index';
+import { closeMenu } from '../../components/NavBar/action';
 
 import CategoriesList from '../../components/CategoriesList/index';
 import './style.css';
 
 class CategoriesCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.hideSidebar = this.hideSidebar.bind(this);
+  }
+
   componentWillMount() {
     const { dispatch } = this.props;
     dispatch(fetchCategories());
@@ -27,6 +38,12 @@ class CategoriesCarousel extends Component {
     return categoriesList;
   }
 
+  hideSidebar() {
+    if (this.props.sideMenuVisible) {
+      this.props.closeMenu();
+    }
+  }
+
   render() {
     const settings = {
       arrows: false,
@@ -41,13 +58,20 @@ class CategoriesCarousel extends Component {
     const categoriesList = this.createCategoriesList(3, 5);
     return (
       <div className="carousel-container">
-        <Slider {...settings}>
-          {categoriesList.map((categoriesChunk, k) => (
-            <div key={Math.random(k)} className="categories">
-              <CategoriesList categoriesChunk={categoriesChunk} />
-            </div>
-          ))}
-        </Slider>
+        <Sidebar.Pushable>
+          {/*          <SideMenu isVisible={this.props.sideMenuVisible} closeMenu={this.props.closeMenu} /> */}
+          <Sidebar.Pusher dimmed={this.props.sideMenuVisible} onClick={this.hideSidebar}>
+            <NavBar />
+
+            <Slider {...settings}>
+              {categoriesList.map((categoriesChunk, k) => (
+                <div key={Math.random(k)} className="categories">
+                  <CategoriesList categoriesChunk={categoriesChunk} />
+                </div>
+              ))}
+            </Slider>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     );
   }
@@ -56,11 +80,14 @@ class CategoriesCarousel extends Component {
 CategoriesCarousel.propTypes = {
   dispatch: PropTypes.func.isRequired,
   categories: PropTypes.arrayOf(categoryPropType).isRequired,
+  sideMenuVisible: PropTypes.bool.isRequired,
+  closeMenu: PropTypes.func.isRequired,
 };
 const mapStateToProps = state => ({
   categories: getCategories(state.categories),
+  sideMenuVisible: state.sideMenuVisible,
 });
 function mapDispatchToProps(dispatch) {
-  return Object.assign({ dispatch }, bindActionCreators({ fetchCategories }, dispatch));
+  return Object.assign({ dispatch }, bindActionCreators({ fetchCategories, closeMenu }, dispatch));
 }
-export default connect(mapStateToProps, mapDispatchToProps)(CategoriesCarousel);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CategoriesCarousel));
