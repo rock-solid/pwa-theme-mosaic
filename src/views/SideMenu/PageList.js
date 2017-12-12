631c36a12002cae14f1bc958e4756fd6fe72be4a
FlorinaIe <florina.ieremciuc@gmail.com>
2017-12-12 12:05:11 +0200

    display page list

---
+++ src/views/SideMenu/PageList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { List, Header } from 'semantic-ui-react';
+
+export default class PageList extends Component {
+  state = { parentId: 0 };
+
+  setParent(parentId) {
+    console.log(parentId);
+    parentId ? this.setState({ parentId }) : '';
+  }
+
+  render() {
+    const parents = this.props.pages.filter(page => page.parent === this.state.parentId);
+    console.log('state >>>>>>>>>> ', this);
+    console.log('parents >>', parents);
+    return (
+      <List divided relaxed>
+        <Header>Go to</Header>
+        {parents.map(page => (
+          <List.Item key={Math.random()}>
+            <List.Icon name="linkify" size="large" verticalAlign="middle" />
+            <List.Content>
+              <List.Header as="a">{page.title.rendered}</List.Header>
+            </List.Content>
+            {page.children.length > 0 ? (
+              <List.Icon link name="triangle right" size="large" verticalAlign="middle" onClick={e => this.setParent(page.id)} />
+            ) : (
+              ''
+            )}
+          </List.Item>
+        ))}
+      </List>
+    );
+  }
+}
