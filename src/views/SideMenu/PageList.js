2017977a262b2129599632c021c1933706ee66c7
FlorinaIe <florina.ieremciuc@gmail.com>
2017-12-13 11:57:19 +0200

    nested page menu <33

--- src/views/SideMenu/PageList.js
+++ src/views/SideMenu/PageList.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import { List, Header } from 'semantic-ui-react';
+import { List, Header, Icon } from 'semantic-ui-react';
 
 export default class PageList extends Component {
-  state = { parentId: 0, parentTitle: '' };
+  state = { parentIds: [], pages: [], parentPageTitle: ['Go to'], goBack: false };
 
-  setParent(parentId, parentTitle) {
-    parentId ? this.setState({ parentId, parentTitle }) : '';
+  componentWillReceiveProps() {
+    this.setState({ parentIds: [...this.state.parentIds, 0] });
+  }
+  setParentsList(parentId, pages, parentPageTitle) {
+    this.setState({
+      parentIds: [...this.state.parentIds, parentId],
+      pages: [...this.state.pages, pages],
+      parentPageTitle: [...this.state.parentPageTitle, parentPageTitle],
+      goBack: false,
+    });
+  }
+  setParentsBackList(wat) {
+    this.state.parentIds.pop();
+    const poppedParentIds = this.state.parentIds;
+    this.state.parentPageTitle.pop();
+    const poppedPageTitles = this.state.parentPageTitle;
+    this.setState({ parentId: poppedParentIds, parentsPageTitle: poppedPageTitles, goBack: true });
   }
 
   render() {
-    const parents = this.props.pages.filter(page => page.parent === this.state.parentId);
+    const checkParent = this.state.parentIds[this.state.parentIds.length - 1];
+    let parents;
+    this.state.goBack === false ? (parents = this.props.pages.filter(page => page.parent === checkParent)) : (parents = this.state.pages.pop());
+
     return (
       <List divided relaxed>
-        <Header>{this.state.parentId === 0 ? 'Go to' : this.state.parentTitle}</Header>
+        {checkParent === 0 ? (
+          <Header>{this.state.parentPageTitle[this.state.parentPageTitle.length - 1]}</Header>
+        ) : (
+          <Header>
+            <Icon name="caret left" onClick={e => this.setParentsBackList()} />
+            {this.state.parentPageTitle[this.state.parentPageTitle.length - 1]}
+          </Header>
+        )}
         {parents.map(page => (
           <List.Item key={Math.random()}>
             <List.Icon name="linkify" size="large" verticalAlign="middle" />
@@ -20,7 +45,13 @@ export default class PageList extends Component {
               <List.Header as="a">{page.title.rendered}</List.Header>
             </List.Content>
             {page.children.length > 0 ? (
-              <List.Icon link name="triangle right" size="large" verticalAlign="middle" onClick={e => this.setParent(page.id, page.title.rendered)} />
+              <List.Icon
+                link
+                name="triangle right"
+                size="large"
+                verticalAlign="middle"
+                onClick={e => this.setParentsList(page.id, parents, page.title.rendered)}
+              />
             ) : (
               ''
             )}
