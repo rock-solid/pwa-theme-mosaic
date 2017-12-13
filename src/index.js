9abd5a72c7d8b947b132c0f55a8c39d800b5089b
FlorinaIe <florina.ieremciuc@gmail.com>
2017-12-13 15:45:28 +0200

    refactor page view route

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './App';
 import CategoriesCarousel from './views/CategoriesCarousel/index';
 import PostsCarousel from './views/PostsCarousel/index';
 import PostView from './views/Post/index';
+import PageView from './views/SideMenu/PageView';
 
 import './index.css';
 
@@ -21,7 +22,7 @@ render(
           <Route path="/categories" component={CategoriesCarousel} />
           <Route path="/category/:categorySlug/:categoryId" component={PostsCarousel} />
           <Route path="/post/:postSlug/:postId" component={PostView} />
-          <Route path="/page/:pageSlug/:pageId" component={CategoriesCarousel} />
+          <Route path="/page/:pageSlug/:pageId" component={PageView} />
         </Switch>
       </App>
     </HashRouter>
