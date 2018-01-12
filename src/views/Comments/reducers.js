f965c03eaba65908a6cc13a8e8ac4a77d91043a4
FlorinaIe <florina.ieremciuc@gmail.com>
2018-01-12 15:30:56 +0200

    get comments

---
+++ src/views/Comments/reducers.js
@@ -0,0 +1,44 @@
+import { combineReducers } from 'redux';
+import Immutable from 'seamless-immutable';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+
+import { REQUEST_COMMENTS, RECEIVE_COMMENTS } from './actions';
+
+export const commentPropType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+});
+
+export const INITIAL_STATE = Immutable({
+  items: [],
+  isFetching: 0,
+});
+
+const items = (state = INITIAL_STATE.items, action) => {
+  switch (action.type) {
+  case REQUEST_COMMENTS:
+    return state;
+  case RECEIVE_COMMENTS:
+    if (Array.isArray(action.comments)) {
+      return _.unionBy(state, action.comments, 'id');
+    }
+    return _.unionBy(state, [action.comments], 'id');
+  default:
+    return state;
+  }
+};
+const isFetching = (state = INITIAL_STATE.isFetching, action) => {
+  switch (action.type) {
+  case REQUEST_COMMENTS:
+    return state + 1;
+  case RECEIVE_COMMENTS:
+    return state - 1;
+  default:
+    return state;
+  }
+};
+
+export const getComments = state => state.items;
+export const getCommentsFetching = state => state.isFetching;
+
+export default combineReducers({ items, isFetching });
