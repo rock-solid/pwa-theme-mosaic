6d599ec64d68942cf33748be82e997b8f277091b
FlorinaIe <florina.ieremciuc@gmail.com>
2018-01-18 15:30:09 +0200

    add comment form component

---
+++ src/components/Form/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Form, Input, Button } from 'semantic-ui-react';
+
+import './style.css';
+
+let CommentForm = (props) => {
+  const { handleSubmit } = props;
+  return (
+    <Form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <Field name="name" component="input" type="text" placeholder="Your name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" placeholder="Your email address" required />
+      </div>
+      <div>
+        <label htmlFor="content">Leave a comment</label>
+        <Field name="content" component="textarea" type="text" placeholder="Tell us what you think..." required />
+      </div>
+      <Button type="submit" color="teal" compact>
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+CommentForm = reduxForm({
+  form: 'comment',
+})(CommentForm);
+
+export default CommentForm;
