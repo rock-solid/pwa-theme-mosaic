780e8b70f8df515c18f53c3759f0ec9f23be6913
FlorinaIe <florina.ieremciuc@gmail.com>
2018-01-25 14:55:56 +0200

    social media app configuration

--- src/views/Post/PostView.js
+++ src/views/Post/PostView.js
@@ -4,6 +4,7 @@ import { Container, Image, Header, Label, Icon, Modal, Transition } from 'semant
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import config from 'react-global-configuration';
 
 import { postPropType } from '../PostsCarousel/reducer';
 import './style.css';
@@ -46,6 +47,10 @@ export default class PostView extends Component {
     // set path routes
     let goBack = {};
     let path = {};
+
+    // social media app configuration
+    const socialMedia = config.get('socialMedia');
+
     if (_.isNil(this.props.category.categorySlug) || _.isNil(this.props.category.categoryId)) {
       goBack = '/';
       path = '/post/' + post.slug + '/' + post.id + '/comments/' + post.comment_status;
@@ -99,15 +104,21 @@ export default class PostView extends Component {
             <Link to={path}>
               <Icon name="comment" size="large" circular inverted color="grey" />
             </Link>
-            <a href={'https://m.facebook.com/sharer.php?u=' + post.link}>
-              <Icon name="facebook f" size="large" circular inverted color="blue" />
-            </a>
-            <a href={'https://twitter.com/intent/tweet?text=' + encodeURIComponent(post.title.rendered) + ' ' + post.link}>
-              <Icon name="twitter" size="large" circular inverted color="teal" />
-            </a>
-            <a href={'https://plus.google.com/share?url=' + post.link}>
-              <Icon name="google plus" size="large" circular inverted color="red" />
-            </a>
+            {socialMedia.facebook ? (
+              <a href={'https://m.facebook.com/sharer.php?u=' + post.link}>
+                <Icon name="facebook f" size="large" circular inverted color="blue" />
+              </a>
+            ) : null}
+            {socialMedia.twitter ? (
+              <a href={'https://twitter.com/intent/tweet?text=' + encodeURIComponent(post.title.rendered) + ' ' + post.link}>
+                <Icon name="twitter" size="large" circular inverted color="teal" />
+              </a>
+            ) : null}
+            {socialMedia.google ? (
+              <a href={'https://plus.google.com/share?url=' + post.link}>
+                <Icon name="google plus" size="large" circular inverted color="red" />
+              </a>
+            ) : null}
           </Modal.Actions>
         </Modal>
       </Container>
