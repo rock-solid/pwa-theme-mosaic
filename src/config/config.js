89b7c4164e371f06bb4ca4a4105712b3594671ba
Alexandra Anghel <alexandra@appticles.com>
2018-04-05 19:08:51 +0300

    Move configuration to window variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,3 @@
-const GLOBAL_VARIABLES = {
-  API_CATEGORIES_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/categories',
-  API_POSTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/posts?_embed=users?_embed=media',
-  API_PAGES_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/pages?_embed=media',
-  API_COMMENTS_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/comments',
-  API_MEDIA_URL: 'http://localhost/wordpress-4.8.3/wordpress/wp-json/wp/v2/media',
-};
+const GLOBAL_VARIABLES = window.appticles.config;
 
 export default GLOBAL_VARIABLES;
