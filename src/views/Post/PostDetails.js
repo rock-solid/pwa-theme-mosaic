5b487ccc266f1373c46b78ee4b36f07fa12dbc80
Alexandra Anghel <alexandra@appticles.com>
2018-04-13 16:46:55 +0300

    Format code

--- src/views/Post/PostDetails.js
+++ src/views/Post/PostDetails.js
@@ -91,14 +91,6 @@ class PostDetails extends Component {
   }
 }
 
-PostDetails.defaultProps = {
-  texts: {
-    TEXTS: {
-      BY_AUTHOR: 'by',
-    },
-  },
-};
-
 PostDetails.propTypes = {
   post: postPropType.isRequired,
   category: PropTypes.shape({
@@ -110,6 +102,14 @@ PostDetails.propTypes = {
       BY_AUTHOR: PropTypes.string,
     }),
   }),
+};
+
+PostDetails.defaultProps = {
+  texts: {
+    TEXTS: {
+      BY_AUTHOR: 'by',
+    },
+  },
 };
 
 export default PostDetails;
