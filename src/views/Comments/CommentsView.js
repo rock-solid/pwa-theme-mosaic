08c51fc4e40c42652130e60789bed90b001a147b
Alexandra Anghel <alexandra@appticles.com>
2018-04-13 16:52:42 +0300

    Refactor comments

--- src/views/Comments/CommentsView.js
+++ src/views/Comments/CommentsView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Comment, Icon, Container, Button, Modal } from 'semantic-ui-react';
+import { Header, Comment, Icon, Container, Button, Modal, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 
@@ -9,14 +9,15 @@ import CommentForm from '../../components/Form/index';
 
 import './style.css';
 
-export default class CommentsView extends Component {
+class CommentsView extends Component {
   constructor(props) {
     super(props);
-    this.submit = this.submit.bind(this);
-    this.showModal = this.showModal.bind(this);
     this.state = {
       isOpen: false,
     };
+
+    this.submit = this.submit.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
 
   submit(values) {
@@ -27,7 +28,7 @@ export default class CommentsView extends Component {
         rendered: values.content,
       },
       date: new Date(),
-      post: this.props.match.params.postId,
+      post: this.props.postId,
     };
     addComment(params);
   }
@@ -37,8 +38,16 @@ export default class CommentsView extends Component {
   }
 
   render() {
+    if (this.props.loading) {
+      return (
+        <Comment.Group className="post-comments">
+          <Loader active />
+        </Comment.Group>
+      );
+    }
+
     return (
-      <Comment.Group>
+      <Comment.Group className="post-comments">
         {this.props.comments.length > 0 ? (
           this.props.comments.map(comment => (
             <Comment key={String(comment.id) + String(comment.content.rendered.length)}>
@@ -52,11 +61,7 @@ export default class CommentsView extends Component {
                 </Comment.Metadata>
                 <Comment.Text dangerouslySetInnerHTML={{ __html: comment.content.rendered }} />
                 <Comment.Actions>
-                  <Comment.Action
-                    onClick={() => {
-                      this.setState({ isOpen: !this.state.isOpen });
-                    }}
-                  >
+                  <Comment.Action onClick={this.showModal}>
                     {this.props.texts.FORMS && this.props.texts.FORMS.REPLY}
                   </Comment.Action>
                 </Comment.Actions>
@@ -64,21 +69,19 @@ export default class CommentsView extends Component {
             </Comment>
           ))
         ) : (
-          <Container className="no-comments">
-            {this.props.match.params.comment_status === 'open'
-              ? this.props.texts.TEXTS && this.props.texts.TEXTS.NO_COMMENTS
-              : this.props.texts.TEXTS && this.props.texts.TEXTS.NO_COMMENTS_SHORT}
-          </Container>
-        )}
-        {this.props.match.params.comment_status === 'open' ? (
-          <Button
-            onClick={() => {
-              this.setState({ isOpen: !this.state.isOpen });
-            }}
-          >
+            <Container className="no-comments">
+              {this.props.commentStatus === 'open'
+                ? this.props.texts.TEXTS && this.props.texts.TEXTS.NO_COMMENTS
+                : this.props.texts.TEXTS && this.props.texts.TEXTS.NO_COMMENTS_SHORT}
+            </Container>
+          )}
+
+        {this.props.commentStatus === 'open' ? (
+          <Button onClick={this.showModal}>
             {this.props.texts.TEXTS && this.props.texts.TEXTS.LEAVE_COMMENTS}
           </Button>
         ) : null}
+
         {this.state.isOpen === true ? (
           <Modal open>
             <Icon name="close" onClick={this.showModal} />
@@ -88,10 +91,28 @@ export default class CommentsView extends Component {
             </Modal.Content>
           </Modal>
         ) : null}
+
       </Comment.Group>
     );
   }
 }
+
+CommentsView.propTypes = {
+  postId: PropTypes.string.isRequired,
+  commentStatus: PropTypes.oneOf(['open', 'closed', 'disabled']).isRequired,
+  comments: PropTypes.arrayOf(commentPropType).isRequired,
+  loading: PropTypes.number.isRequired,
+  texts: PropTypes.shape({
+    FORMS: PropTypes.shape({
+      REPLY: PropTypes.string,
+    }),
+    TEXTS: PropTypes.shape({
+      NO_COMMENTS: PropTypes.string,
+      NO_COMMENTS_SHORT: PropTypes.string,
+      LEAVE_COMMENTS: PropTypes.string,
+    }),
+  }),
+};
 
 CommentsView.defaultProps = {
   texts: {
@@ -105,25 +126,5 @@ CommentsView.defaultProps = {
     },
   },
 };
-CommentsView.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      postSlug: PropTypes.string.isRequired,
-      postId: PropTypes.string.isRequired,
-      categorySlug: PropTypes.string,
-      categoryId: PropTypes.string,
-      comment_status: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-  comments: PropTypes.arrayOf(commentPropType).isRequired,
-  texts: PropTypes.shape({
-    FORMS: PropTypes.shape({
-      REPLY: PropTypes.string,
-    }),
-    TEXTS: PropTypes.shape({
-      NO_COMMENTS: PropTypes.string,
-      NO_COMMENTS_SHORT: PropTypes.string,
-      LEAVE_COMMENTS: PropTypes.string,
-    }),
-  }),
-};
+
+export default CommentsView;
--- src/views/Comments/index.js
+++ src/views/Comments/index.js
@@ -1,13 +1,12 @@
-import 'react-router-modal/css/react-router-modal.css';
-
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Loader, Comment, Header, Icon } from 'semantic-ui-react';
+import { Loader, Header, Icon } from 'semantic-ui-react';
 import _ from 'lodash';
 import ReactPullToRefresh from 'react-pull-to-refresh';
+import 'react-router-modal/css/react-router-modal.css';
 
 import { fetchComments } from './actions';
 import { getComments, getCommentsFetching, commentPropType } from './reducers';
@@ -22,16 +21,17 @@ class Comments extends Component {
     super(props);
     this.handleRefresh = this.handleRefresh.bind(this);
   }
+
   componentWillMount() {
     const { dispatch } = this.props;
-    dispatch(fetchComments({ postId: this.props.match.params.postId }));
+    dispatch(fetchComments({ postId: this.props.match.params.postId, per_page: 100 }));
     dispatch(fetchTranslations);
   }
 
   handleRefresh(resolve, reject) {
     const { dispatch } = this.props;
 
-    if (dispatch(fetchComments({ postId: this.props.match.params.postId }))) {
+    if (dispatch(fetchComments({ postId: this.props.match.params.postId, per_page: 100 }))) {
       resolve();
     } else {
       reject();
@@ -41,57 +41,38 @@ class Comments extends Component {
   render() {
     const comm = this.props.comments.filter(comment => comment.post === Number(this.props.match.params.postId));
 
-    let path = {};
-    if (_.isNil(this.props.match.params.categorySlug) || _.isNil(this.props.match.params.categoryId)) {
-      path = '/post/' + this.props.match.params.postSlug + '/' + this.props.match.params.postId;
-    } else {
-      path =
-        '/category/' +
-        this.props.match.params.categorySlug +
-        '/' +
-        this.props.match.params.categoryId +
-        '/post/' +
-        this.props.match.params.postSlug +
-        '/' +
-        this.props.match.params.postId;
+    let path = '';
+    if (!_.isNil(this.props.match.params.categorySlug) &&
+      !_.isNil(this.props.match.params.categoryId)) {
+      path = '/category/' + this.props.match.params.categorySlug + '/' + this.props.match.params.categoryId;
     }
 
-    if (this.props.loading === 1 && this.props.loadingTranslations === 1) {
-      return (
-        <Comment.Group>
-          <Header as="h3" block>
-            {this.props.translations.TEXTS && this.props.translations.TEXTS.COMMENTS}
-            <Link to={path}>
-              <Icon name="close" />
-            </Link>
-          </Header>
-          <Loader active />
-        </Comment.Group>
-      );
-    }
+    path = path + '/post/' + this.props.match.params.postSlug + '/' + this.props.match.params.postId;
 
     return (
-      <Comment.Group>
-        <Header as="h3" block>
+      <div>
+        <Header as="h3" block className="comments-header">
           {this.props.translations.TEXTS && this.props.translations.TEXTS.COMMENTS}
           <Link to={path}>
             <Icon name="close" />
           </Link>
         </Header>
-        <ReactPullToRefresh onRefresh={this.handleRefresh}>
-          <CommentsView comments={comm} match={this.props.match} texts={this.props.translations} />
-        </ReactPullToRefresh>
-      </Comment.Group>
+        {this.props.loading === 1 && this.props.loadingTranslations === 1 ?
+          <Loader active /> :
+          (<ReactPullToRefresh onRefresh={this.handleRefresh}>
+            <CommentsView
+              comments={comm}
+              loading={this.props.loading}
+              postId={this.props.match.params.postId}
+              commentStatus={this.props.match.params.commentStatus}
+              texts={this.props.translations}
+            />
+          </ReactPullToRefresh>)}
+      </div>
     );
   }
 }
-Comments.defaultProps = {
-  translations: {
-    TEXTS: {
-      COMMENTS: 'Comments',
-    },
-  },
-};
+
 Comments.propTypes = {
   dispatch: PropTypes.func.isRequired,
   match: PropTypes.shape({
@@ -100,6 +81,7 @@ Comments.propTypes = {
       categorySlug: PropTypes.string.isRequired,
       postId: PropTypes.string.isRequired,
       postSlug: PropTypes.string.isRequired,
+      commentStatus: PropTypes.oneOf(['open', 'closed', 'disabled']).isRequired,
     }).isRequired,
   }).isRequired,
   loading: PropTypes.number.isRequired,
@@ -110,6 +92,14 @@ Comments.propTypes = {
       COMMENTS: PropTypes.string,
     }),
   }),
+};
+
+Comments.defaultProps = {
+  translations: {
+    TEXTS: {
+      COMMENTS: 'Comments',
+    },
+  },
 };
 
 const mapStateToProps = state => ({
