8cfb257adc002d359e7f402d97d51e5d9724d0da
Alexandra Anghel <alexandra@appticles.com>
2018-07-02 13:52:11 +0300

    Add gulp script for concatenating scss files

---
+++ gulptools/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const pump = require('pump');
+const replace = require('gulp-replace');
+
+gulp.task('default', ['build-custom-sass']);
+
+gulp.task('build-custom-sass', () => {
+  const orderedPaths = require('./paths.json').paths;
+
+  pump([
+    gulp.src(orderedPaths),
+    concat('customizable.scss'),
+    replace(/@import ["a-z_.\/,\s;]+;/g, ''),
+    gulp.dest('../build/'),
+  ]);
+});
