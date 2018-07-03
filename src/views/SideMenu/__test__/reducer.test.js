e870798f9dd2ac5ba652bd5699f8023c68f03afc
Alexandra Anghel <alexandra@appticles.com>
2018-07-03 20:17:12 +0300

    Update pages reducer tests

--- src/views/SideMenu/__test__/reducer.test.js
+++ src/views/SideMenu/__test__/reducer.test.js
@@ -1,7 +1,7 @@
 import pages, { INITIAL_STATE, getPages, getPagesFetching } from '../reducer';
 import { requestPages, receivePages } from '../action';
 
-describe('Posts reducer', () => {
+describe('Pages reducer', () => {
   it('returns same state on an unhandled action', () => {
     expect(pages(INITIAL_STATE, { type: '_NULL' })).toMatchSnapshot();
   });
@@ -12,15 +12,22 @@ describe('Posts reducer', () => {
         {
           id: 1,
           name: 'Nice page',
+          content: {
+            protected: false,
+          },
         },
         {
           id: 2,
           name: 'Nicer page',
+          content: {
+            protected: false,
+          },
         },
       ],
     };
     expect(pages(pageState, requestPages())).toMatchSnapshot();
   });
+
   it('handles RECEIVE_PAGES action case 1', () => {
     const pageState = {
       isFetching: 1,
@@ -28,19 +35,30 @@ describe('Posts reducer', () => {
         {
           id: 1,
           name: 'Nice page',
+          content: {
+            protected: false,
+          },
         },
         {
           id: 2,
           name: 'Nicer page',
+          content: {
+            protected: false,
+          },
         },
       ],
     };
+
     const pageList = {
       id: 3,
       name: 'Nicest page',
+      content: {
+        protected: false,
+      },
     };
     expect(pages(pageState, receivePages(pageList))).toMatchSnapshot();
   });
+
   it('handles RECEIVE_PAGES action case 2', () => {
     const pageState = {
       isFetching: 1,
@@ -48,10 +66,16 @@ describe('Posts reducer', () => {
         {
           id: 1,
           name: 'Nice page',
+          content: {
+            protected: false,
+          },
         },
         {
           id: 2,
           name: 'Nicer page',
+          content: {
+            protected: false,
+          },
         },
       ],
     };
@@ -59,6 +83,42 @@ describe('Posts reducer', () => {
       {
         id: 3,
         name: 'Nicest page',
+        content: {
+          protected: false,
+        },
+      },
+    ];
+    expect(pages(pageState, receivePages(pageList))).toMatchSnapshot();
+  });
+
+  it('handles RECEIVE_PAGES with password protected pages keeps same state', () => {
+    const pageState = {
+      isFetching: 1,
+      items: [
+        {
+          id: 1,
+          name: 'Nice page',
+          content: {
+            protected: false,
+          },
+        },
+        {
+          id: 2,
+          name: 'Nicer page',
+          content: {
+            protected: false,
+          },
+        },
+      ],
+    };
+
+    const pageList = [
+      {
+        id: 3,
+        name: 'Nicest page',
+        content: {
+          protected: true,
+        },
       },
     ];
     expect(pages(pageState, receivePages(pageList))).toMatchSnapshot();
@@ -68,10 +128,12 @@ describe('Posts reducer', () => {
   it('getPagesFetching returns state.isFetching', () => {
     expect(getPagesFetching(INITIAL_STATE)).toBe(0);
   });
+
   it('getPages returns state.items', () => {
     const state = {
       items: [
         {
+          id: 1,
           name: 'Some page',
         },
       ],
